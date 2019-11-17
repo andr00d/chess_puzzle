4f1770643893f1e63274e3e1e31bab0e8fbc3290
Roel <roel.devries@student.fontys.nl>
2019-11-17 13:26:03 +0100

    pawn-RED

--- test/pawnTest.cpp
+++ test/pawnTest.cpp
@@ -29,6 +29,14 @@ TEST_F(pawnTest, getSetXPosition)
 	ASSERT_EQ(5, P->getXPos());
 }
 
+TEST_F(pawnTest, getSetYPosition)
+{
+	ASSERT_EQ(0, P->getYPos());
+
+	P->setPosition(5, 6);
+	ASSERT_EQ(5, P->getYPos());
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
